7f0029d1bf32ffefae909ce78df9824439cb6581
Menzy <juhbeall@gmail.com>
2025-09-23 14:58:59 -0300

    Função displayMenu

---
+++ funcoes.py
@@ -0,0 +1,11 @@
+def displayMenu():
+    return print("=== Planejador de Eventos do Campus ===\n" \
+    "1. Adicionar Evento\n" \
+    "2. Ver Todos os Eventos\n" \
+    "3. Filtrar por Categoria\n" \
+    "4. Marcar Evento como Participado\n" \
+    "5. Gerar Relatório\n" \
+    "6. Sair")
+
+
+displayMenu()
