d4b5013d8b3a0c0e54d6aee738c3709ad5a70b15
Menzy <juhbeall@gmail.com>
2025-09-24 12:16:14 -0300

    Removi o Match case e refiz a estrutura

--- funcoes.py
+++ funcoes.py
@@ -1,3 +1,4 @@
+
 def displayMenu():
     print("=== Planejador de Eventos do Campus ===\n" \
     "1. Adicionar Evento\n" \
@@ -7,47 +8,60 @@ def displayMenu():
     "5. Gerar Relatório\n" \
     "6. Sair")
 
-def getEscolhaDoUsuario(op): # op = opção
-
-    match op:
-        case 1:
-            nome = input("Nome do Evento: ")
-            data = input("Data (AAAA=MM=DD): ")
-            local = input("Local: ")
-            categoria = input("Categoria: ")
-
-            #adicionarEvento(listaEventos, nome, data, local, categoria)
-            print("\nEvento adicionado com sucesso!")
-
-        case 2:
-            #listarEventos(listaEventos)
-        
-        #case 3:
-            #categoria = input("Digite qual categoria deseja buscar: ")
-            #filtrarEventosPorCategoria(listaEventos, categoria)
-        
-        #case 4:
-            #marcarEventoAtendido(listaEventos, id)
-
-        #case 5:
-            #gerarRelatorio(listaEventos)
-
-        #case 6:
-
-
-    #return 
-
+def getEscolhaDoUsuario(): # op = opção
+    op = (input("Escolha uma opção: "))
+    if op.isnumeric():
+        op = int(op)
+        return op
+    else:
+        print("Opção inexistente, tente novamente!")
 
 def filtrarEventosPorCategoria(listaEventos, categoria):
 
     return
 
-def marcarEventoAtendido(listaEventos, id):
-
-    return
+#def marcarEventoAtendido(listaEventos, id):
 
 
-def gerarRelatorio(listaEventos):
-    return 
+    #return
+
+
+#def gerarRelatorio(listaEventos):
+    
+    #return 
 
 displayMenu()
+listaEventos = []
+op = 0
+while op != 6:
+
+    op = getEscolhaDoUsuario()
+
+
+    if op == 1:
+        nome = input("Nome do Evento: ")
+        data = input("Data (AAAA=MM=DD): ")
+        local = input("Local: ")
+        categoria = input("Categoria: ")
+            
+        #adicionarEvento(listaEventos, nome, data, local, categoria)
+        print("\nEvento adicionado com sucesso!")
+
+    #elif op == 2:
+    #listarEventos(listaEventos)
+
+    elif op == 3:
+        categoria = input("Digite a categoria de eventos que deseja filtrar: ")
+        filtrarEventosPorCategoria(listaEventos, categoria)
+
+    elif op == 4:
+        id = int(input("Digite o ID do evento que deseja marcar como atendido: "))
+        #marcarEventoAtendido(listaEventos, id)
+
+    #elif op == 5:
+        #gerarRelatorio(listaEventos)
+
+    else:
+        print("Opção inexistente, tente novamente!")
+
+print("Programa encerrado!")
